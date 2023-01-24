144221ad980708a09f2d8c237d198c43c4853d39
melyanavila <minervadeapolo@gmail.com>
2023-01-24 13:00:41 -0600

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function ingresar(){
-    const valorUsuario= document.getElementById('user').value;
-    const valorEmail= document.getElementById('email').value;
-    const valorPassword= document.getElementById('password').value;
 
-    const object= {
+    const valorUsuario= document.getElementById("user").value;
+    const valorEmail= document.getElementById("email").value;
+    const valorPassword= document.getElementById("password").value;
+
+    const object={
         user: valorUsuario,
         email: valorEmail,
         password: valorPassword,
     }
 
     console.log(object);
+
 }
